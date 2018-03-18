d1a5d96f4a31c7f41b7990a910ac1ee78a55ffaf
foxbrush <fischer.json@gmail.com>
2018-03-18 19:10:01 +0800

    Done with TodoList styles

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,16 @@ import "./TodoList.css";
 
 class TodoItem extends Component {
   render() {
+    let className = "todo-list__todo-item ";
+    if (this.props.isDone) {
+      className += "todo-done";
+    }
     return (
-      <li className="todo-list__item">
-        
+      <li className={className}>
+        <button className="todo-item__done-btn"></button>
+        <p className="todo-item__content">The box blur outputs the average of the pixel values inside the convolution matrix. The way to do that is to create a convolution matrix of size NxN where each of the weights is 1 / (NxN). The box blur outputs the average of the pixel values inside the convolution matrix. The way to do that is to create a convolution matrix of size NxN where each of the weights is 1 / (NxN).
+        </p>
+        <button className="todo-item__del-btn"></button>
       </li>
     );
   }
@@ -16,6 +23,8 @@ class TodoList extends Component {
   render() {
     return (
       <ul className="todo-list">
+        <TodoItem />
+        <TodoItem isDone="true" />
         <TodoItem />
       </ul>
     );
